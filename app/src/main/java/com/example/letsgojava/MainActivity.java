54e42649aca7b33c3e4aa0b66927ccca7113fefe
Unekwe Francis <unekwenchekwube@gmail.com>
2022-04-10 07:54:09 +0100

    learnt about adding a global event listener to  a class and using switch statements for different buttons

--- app/src/main/java/com/example/letsgojava/MainActivity.java
+++ app/src/main/java/com/example/letsgojava/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.letsgojava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView txtView;
+    private TextView submitBtn;
+    private TextView nameText;
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.submitBtn:
+                Toast.makeText(this, "Helllllo! from mofuckin' toast", Toast.LENGTH_SHORT).show();
+                txtView.setText(nameText.getText().toString());
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         txtView = findViewById(R.id.textView);
+         submitBtn  = findViewById(R.id.submitBtn);
+         nameText = findViewById(R.id.nameText);
+         submitBtn.setOnClickListener(this);
+
     }
+
+
 }
