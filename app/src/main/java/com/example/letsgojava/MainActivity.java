25eaa7b43bbbab7e770c0dcbd2f58a18f38ff0ff
Unekwe Francis <unekwenchekwube@gmail.com>
2022-04-10 15:18:17 +0100

    learnt about snackbars from material design

--- app/src/main/java/com/example/letsgojava/MainActivity.java
+++ app/src/main/java/com/example/letsgojava/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.letsgojava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView txtView;
     private TextView submitBtn;
     private TextView nameText;
+    private RelativeLayout parentElement;
+    private FloatingActionButton fab;
+
     @Override
     public void onClick(View view) {
         switch (view.getId()){
@@ -31,7 +39,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
          submitBtn  = findViewById(R.id.submitBtn);
          nameText = findViewById(R.id.nameText);
          submitBtn.setOnClickListener(this);
+         fab = findViewById(R.id.fab);
+        parentElement = findViewById(R.id.parentElement);
+      fab.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              showSnack();
+          }
+      });
+    }
 
+    public void showSnack(){
+        Snackbar.make(parentElement,"An error occurred!.",Snackbar.LENGTH_INDEFINITE)
+                .setAction("Retry", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Retrying....", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .show();
     }
 
 
